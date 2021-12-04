3ccfd2d6874f44c9b0c39fd7c904aa909b05f217
fitosegrera <fitosegrera@gmail.com>
2021-12-04 11:49:37 -0500

    added Vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,12 @@
+import vercel from "@sveltejs/adapter-vercel";
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+  kit: {
+    adapter: vercel(),
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: "#svelte",
+  },
 };
 
 export default config;
